9b5abd15214a22cd8cd5e838ff48029996d74180
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-06-12 18:53:35 +1200

    Fixed github-release download script.

--- src/ps/install-github-release.ps1
+++ src/ps/install-github-release.ps1
@@ -4,49 +4,67 @@ param(
 
 Add-Type -As System.IO.Compression.FileSystem
 
-# Download the file. Currently there seems to be no way to determine what the latest release is so let just pick one
-$url = "https://github.com/aktau/github-release/releases/download/v0.5/windows-amd64-github-release.zip"
+# Store the current directory so that we can get back there
+$currentDir = $pwd
 
-
+# Create a temp directory to download the file into and then to unzip it
 $tempDirName = [system.guid]::newguid().tostring()
-
 $tempDir = Join-Path $env:TEMP $tempDirName
-new-item -type directory -name $tempDir
-set-location $tempDir
 
-$zipFile = Join-Path $tempDir 'github-release.zip'
+"Creating temp directory at: $tempDir ..."
+New-Item -ItemType directory -Path $tempDir | Out-Null
 
 Try
 {
+    $zipFile = Join-Path $tempDir 'github-release.zip'
+
+    # Download the file. Currently there seems to be no way to determine what the latest release is so let just pick one
+    $url = "https://github.com/aktau/github-release/releases/download/v0.5/windows-amd64-github-release.zip"
+    
+    "Downloading file from: $url"
+    "Saving to: $zipFile"
+    "..."
     $webclient = New-Object System.Net.WebClient
     $webclient.DownloadFile($url,$zipFile)
 
     # Extract the files
+    "Extracting $zipFile ..."
     [IO.Compression.ZipFile]::ExtractToDirectory( $zipFile, $tempDir )
     
-    if(!(Test-Path -Path $installPath )){
-        New-Item -ItemType directory -Path $installPath
+    # Copy only the executable and put it in a known path
+    "Copying github-release.exe to $installPath ..."
+    if (!(Test-Path -Path $installPath ))
+    {
+        New-Item -ItemType directory -Path $installPath | Out-Null
     }
     
-    Copy-Item (Join-Path $tempDir "bin\windows\amd64") (Join-Path $installPath "github-release.exe")
+    Copy-Item (Join-Path $tempDir "bin\windows\amd64\github-release.exe") (Join-Path $installPath "github-release.exe")
 
-    # Add to PATH
+    # Add to user PATH
+    "Adding $installDir to the user PATH environment variable ..."
+    
     # Get the current search path from the environment keys in the registry.
-    $OldPath=[environment]::GetEnvironmentVariable("PATH", "User")
+    $oldPath=[environment]::GetEnvironmentVariable("PATH", "User")
 
     # See if the new Folder is already in the path.
-    IF ($ENV:PATH | Select-String -SimpleMatch $installPath)
+    if ($oldPath | Select-String -SimpleMatch $installPath)
     { 
-        Return 
+        return 
     }
 
     # Set the New Path
-    $NewPath=$OldPath + ";" + $installPath
-    [Environment]::SetEnvironmentVariable("PATH", $NewPath, "User")
+    $newPath=$oldPath + ";" + $installPath
+    [Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
 }
 Finally
 {
-    If (Test-Path $tempDir){
-        Remove-Item $tempDir -Force
+    sl $currentDir
+    
+    "Removing $tempDir ..."
+    if (Test-Path $tempDir)
+    {
+        Remove-Item $tempDir -Force -Recurse | Out-Null
     }
 }
+
+"Script completed"
